dae95ad77a115c3b7c9da5d14a4e5a4c73701117
Paul Giglio <gigliop5000583@gmail.com>
2022-12-02 16:43:35 -0500

    ScoreModel storage

--- src/model/ScoreModel.java
+++ src/model/ScoreModel.java
@@ -7,4 +7,18 @@ public class ScoreModel {
         this.scoreArray = new int[17];
     }
 
+    public int getScoreArray(int i) {
+        return scoreArray[i];
+    }
+
+    public void setArrayValue(int x, int y) {
+        scoreArray[x] = y;
+    }
+
+    public void defaultScore() {
+        for (int i = 0; i < 17; i++) {
+            scoreArray[i] = -1;
+        }
+    }
+
 }
