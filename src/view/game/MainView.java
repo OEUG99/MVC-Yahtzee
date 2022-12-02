8a18d74ab087893dd7e173428c7a4192556bc586
O. Eugenio <ome18@fsu.edu>
2022-12-02 17:54:19 -0500

    Fixed a bug where the game would display for mili-seconds before the launcher started.

--- src/view/game/MainView.java
+++ src/view/game/MainView.java
@@ -26,6 +26,7 @@ public class MainView extends JFrame{
 
         this.scoreboardView = new ScoreboardView();
         add(scoreboardView, BorderLayout.CENTER);
+        setVisible(false);
     }
 
     public DiceContainerView getDiceContainer() {
